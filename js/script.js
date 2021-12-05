9a1b7884845563e5d3cb0c2330d38745ee75622a
Dawid Wolas <vrplayerek@gmail.com>
2021-12-05 22:04:01 +0100

    add active to links

--- js/script.js
+++ js/script.js
@@ -1,23 +1,21 @@
 "use strict";
 
 function titleClickHandler(event) {
+  const clickedElement = this;
   console.log("Link was clicked!", event);
 
-  /* remove class 'active' from all article links  */
-  const activeArticles = document.querySelectorAll(".post");
-
-  for (let activeArticle of activeArticles) {
-    activeArticle.classList.remove("active");
-  }
-
-  /* add class 'active' to the clicked link */
-
-  /* remove class 'active' from all articles */
+  /* [DONE]remove class 'active' from all article links  */
   const activeLinks = document.querySelectorAll(".titles a.active");
 
   for (let activeLink of activeLinks) {
     activeLink.classList.remove("active");
   }
+
+  /*[IN PROGRESS] add class 'active' to the clicked link */
+  clickedElement.classList.add("active");
+  console.log("clickedElement:", clickedElement);
+
+  /*[DONE] remove class 'active' from all articles */
 
   /* get 'href' attribute from the clicked link */
 
