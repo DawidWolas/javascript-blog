0c32799426bc88b573d47b568c7e9b1baac86f67
Dawid Wolas <vrplayerek@gmail.com>
2021-12-05 22:46:37 +0100

    add active const

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 "use strict";
 
 function titleClickHandler(event) {
+  event.preventDefault();
   const clickedElement = this;
   console.log("Link was clicked!", event);
 
@@ -11,17 +12,23 @@ function titleClickHandler(event) {
     activeLink.classList.remove("active");
   }
 
-  /*[IN PROGRESS] add class 'active' to the clicked link */
+  /*[Done] add class 'active' to the clicked link */
   clickedElement.classList.add("active");
   console.log("clickedElement:", clickedElement);
 
   /*[DONE] remove class 'active' from all articles */
 
-  /* get 'href' attribute from the clicked link */
+  /*[DONE] get 'href' attribute from the clicked link */
+
+  const articleSelector = clickedElement.getAttribute("href");
+  console.log(articleSelector);
 
   /* find the correct article using the selector (value of 'href' attribute) */
+  const targetArticle = document.querySelector("href");
+  console.log(targetArticle);
 
   /* add class 'active' to the correct article */
+  targetArticle.classList.add("active");
 }
 
 const links = document.querySelectorAll(".titles a");
