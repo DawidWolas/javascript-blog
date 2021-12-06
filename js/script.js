927ab6c8952f49b47089eb2078d7c2f7ea95a4d2
Dawid Wolas <vrplayerek@gmail.com>
2021-12-06 15:31:14 +0100

    finish ClickHandler

--- js/script.js
+++ js/script.js
@@ -12,23 +12,27 @@ function titleClickHandler(event) {
     activeLink.classList.remove("active");
   }
 
-  /*[Done] add class 'active' to the clicked link */
-  clickedElement.classList.add("active");
+  /*[Done] add class 'active' to thcle clicked link */
+
   console.log("clickedElement:", clickedElement);
-
+  clickedElement.classList.add("active");
   /*[DONE] remove class 'active' from all articles */
+  const activeArticles = document.querySelectorAll(".active");
 
+  for (let activeArticle of activeArticles) {
+    activeArticle.classList.remove("active");
+  }
   /*[DONE] get 'href' attribute from the clicked link */
 
   const articleSelector = clickedElement.getAttribute("href");
-  console.log(articleSelector);
-
+  console.log("articleSelector", articleSelector);
   /* find the correct article using the selector (value of 'href' attribute) */
-  const targetArticle = document.querySelector("href");
+  const targetArticle = document.querySelector(articleSelector);
   console.log(targetArticle);
 
   /* add class 'active' to the correct article */
   targetArticle.classList.add("active");
+  console.log("clickedElement:", targetArticle);
 }
 
 const links = document.querySelectorAll(".titles a");
